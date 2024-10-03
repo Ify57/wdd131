afff2014ea6c4b6ac26c95aa9666c630232c4c05
Ify57 <ole24001@byui.edu>
2024-10-03 11:42:34 +0100

    Add files via upload

---
+++ scripts/getdates.js
@@ -0,0 +1,8 @@
+let date = new Date();
+const year = document.getElementById('year').innerHTML = date.getFullYear();
+const day = document.getElementById('day').innerHTML = date.getDay()
+const month = document.getElementById('month').innerHTML = date.getMonth()
+const year2 = document.getElementById('year2').innerHTML = date.getFullYear()
+const hour = document.getElementById('hour').innerHTML = date.getHours()
+const minute = document.getElementById('minute').innerHTML = date.getMinutes()
+const seconds = document.getElementById('seconds').innerHTML = date.getSeconds()
